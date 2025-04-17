744c7a9889c4eda36db708854b5ed59da483e409
unknown <yamazake@yeah.net>
2025-04-17 17:00:59 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
@@ -26,11 +28,10 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/products"
-            target="_blank"
-            rel="noopener noreferrer"
+           
           >
             <Image
               className="dark:invert"
@@ -40,23 +41,21 @@ export default function Home() {
               height={20}
             />
             Explore Products
-          </a>
-          <a
+          </Link>
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[198px]"
             href="mailto:info@tfcomposite.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            
           >
             Contact Us
-          </a>
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="/products"
-          target="_blank"
-          rel="noopener noreferrer"
+         
         >
           <Image
             aria-hidden
@@ -66,12 +65,11 @@ export default function Home() {
             height={16}
           />
           FRP Moulded Grating
-        </a>
-        <a
+        </Link>
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href=""
-          target="_blank"
-          rel="noopener noreferrer"
+          
         >
           <Image
             aria-hidden
@@ -81,7 +79,7 @@ export default function Home() {
             height={16}
           />
           FRP Pultruded Grating
-        </a>
+        </Link>
       </footer>
     </div>
   );
